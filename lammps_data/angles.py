1dac7011be4750c6de5506805429007c6e9100ea
kbsezginel <kbs37@pitt.edu>
2017-05-23 15:09:04 -0400

    Add angle finding method and tests.

--- lammps_data/angles.py
+++ lammps_data/angles.py
@@ -2,5 +2,17 @@
 
 def get_angles(bonds):
     """ Iterate through bonds to get angles.
+
+    Bonds should contain no duplicates.
     """
-    return []
+    angles = []
+    for i1, b1 in enumerate(bonds):
+        for i2, b2 in enumerate(bonds):
+            if i2 > i1:
+                shared_atom = list(set(b1) & set(b2))
+                if len(shared_atom) > 0:
+                    atom1 = [b for b in b1 if b != shared_atom[0]][0]
+                    atom2 = [b for b in b2 if b != shared_atom[0]][0]
+                    other_atoms = sorted([atom1, atom2])
+                    angles.append((other_atoms[0], shared_atom[0], other_atoms[1]))
+    return sorted(angles)
