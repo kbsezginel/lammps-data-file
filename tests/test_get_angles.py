f12579931f11bcffcec2d7fa68ceed98d10e6429
kbsezginel <kbs37@pitt.edu>
2017-05-25 18:15:10 -0400

    PEP8 formatting for tests.

--- tests/test_get_angles.py
+++ tests/test_get_angles.py
@@ -5,9 +5,11 @@ def test_separate_diatomic_molecules_should_have_no_angles():
     bonds = [(0, 1), (2, 3)]
     assert get_angles(bonds) == []
 
+
 def test_molecule_with_two_bonds_should_have_one_angle():
     bonds = [(0, 1), (1, 2)]
     assert get_angles(bonds) == [(0, 1, 2)]
+
 
 def test_different_order_of_bond_tuples_should_return_same_order_within_angle_tuples():
     bonds = [(0, 1), (1, 2)]
@@ -15,11 +17,13 @@ def test_different_order_of_bond_tuples_should_return_same_order_within_angle_tu
     bonds = [(1, 2), (0, 1)]
     assert get_angles(bonds) == [(0, 1, 2)]
 
+
 def test_different_order_of_bond_tuples_should_return_same_order_of_angle_tuples():
     bonds = [(0, 1), (1, 2), (1, 3)]
     assert get_angles(bonds) == [(0, 1, 2), (0, 1, 3), (2, 1, 3)]
     bonds = [(1, 2), (0, 1), (1, 3)]
     assert get_angles(bonds) == [(0, 1, 2), (0, 1, 3), (2, 1, 3)]
+
 
 def test_tetrahedral_molecule_should_have_six_angles():
     bonds = [(0, 1), (0, 2), (0, 3), (0, 4)]
