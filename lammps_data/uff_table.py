3e7e651875341d9a9ebea40019c584dce1ee9b4f
kbsezginel <kbs37@pitt.edu>
2017-06-13 14:51:10 -0400

    Add UffTable load and lookup methods, fix uff table tests.
    Removed one test that checks the length of returned row. Since we will always return one single row and this is already checked in lookup method there is no need for an additional test.
    
    Added docstring for exceptions.
    
    Added lookupo and load methods for uff table read table and return rows for given element, coordination number and angle. The exceptions are raised in lookup method for different cases.

--- lammps_data/uff_table.py
+++ lammps_data/uff_table.py
@@ -1,49 +1,79 @@
+import csv
+import math
 
 
 class UffTable:
-    def __init__(self):
-        return self
+    def __init__(self, uff_file='uff-parameters.csv'):
+        self.load(uff_file)
 
-    def lookup(element, n_bonds, angle, tolerance=10):
+    def load(self, uff_file='uff-parameters.csv'):
+        """
+        Load UFF parameters file.
+        """
+        uff = []
+        with open(uff_file, 'r') as csvfile:
+            csvreader = csv.reader(csvfile, delimiter=',')
+            for row in csvreader:
+                uff.append(row)
+
+        self.parameters = dict()
+        for atom_data in uff[2:]:
+            row = dict(type=atom_data[0],
+                       element=atom_data[1],
+                       coordination=int(atom_data[2]),
+                       bond=float(atom_data[3]),
+                       angle=float(atom_data[4]),
+                       distance=float(atom_data[5]),
+                       energy=float(atom_data[6]),
+                       scale=float(atom_data[7]),
+                       charge=float(atom_data[8]))
+
+            if row['element'] not in self.parameters:
+                self.parameters[row['element']] = {}
+            if row['coordination'] not in self.parameters[row['element']]:
+                self.parameters[row['element']][row['coordination']] = {}
+            if not self.parameters[row['element']][row['coordination']]:
+                self.parameters[row['element']][row['coordination']] = []
+
+            self.parameters[row['element']][row['coordination']].append(row)
+
+    def lookup(self, element, n_bonds, angle, tolerance=10):
         """
         Look up element from UFF table by element name, number of bonds and angle.
         For angle matching absolute tolerance can be adjusted using tolerance keyword argument.
         """
-        element_rows = self.table[element]
+        if element not in self.parameters:
+            raise NonExistentElementError('Element not found in table')
+        elif n_bonds not in self.parameters[element]:
+            raise NonExistentCoordinationError('Coordination of %s not found for element %s' % (str(n_bonds), element))
+        else:
+            rows = []
+            for r in self.parameters[element][n_bonds]:
+                if math.isclose(r['angle'], angle, abs_tol=tolerance):
+                    rows.append(r)
+            if len(rows) == 0:
+                raise NonExistentAngleError('Angle of %s not found for coordination %s of element %s' % (str(angle), str(n_bonds), element))
+            elif len(rows) > 1:
+                raise LookupMultiplicityError('Multiple (%i) angles found for tolerance of: %s' % (len(rows), str(tolerance)))
+            else:
+                return rows[0]
 
 
 class NonExistentElementError(Exception):
+    """ Raised when input element cannot be found in UFF parameters table """
     pass
 
 
 class NonExistentCoordinationError(Exception):
+    """ Raised when input coordination for input element cannot be found in UFF table """
     pass
 
 
 class NonExistentAngleError(Exception):
+    """ Raised when input angle for element and coordination is not within the tolerance value """
     pass
 
 
 class LookupMultiplicityError(Exception):
+    """ Raised when multiple rows are to be returned """
     pass
-"""
-
-1. Center atom
-2. Coordination number
-3. Lookup all angle within tolerance of angle
-- O rows: error
-- 1 row: return rows
-- >1 rows: error
-
-Remove rows with same coordination numbers and angles
-Add rows for square planar geometry for 180 degree angles
-
-Warning for same atom being assigned multiple atom types
-
-3 ways to assign:
-- All UFF parameters
-- All distance and angle from cif
-- Distance and angle from UFF to calculate force constants, equilibrium cif values for other terms
-
-Recognize symmetrical copies and making sure we use same parameters for those
-"""
