09eb9b99b79321097aceea461dcb036d35974769
kbsezginel <kbs37@pitt.edu>
2017-06-13 14:51:10 -0400

    Add UffTable load and lookup methods, fix uff table tests.
    Removed one test that checks the length of returned row. Since we will always return one single row and this is already checked in lookup method there is no need for an additional test.
    
    Added docstring for exceptions.
    
    Added lookupo and load methods for uff table read table and return rows for given element, coordination number and angle. The exceptions are raised in lookup method for different cases.

--- tests/test_uff_table.py
+++ tests/test_uff_table.py
@@ -1,6 +1,8 @@
 import pytest
-from lammps_data.uff_table import UffTable, NonExistentElementError, \
-                                  NonExistentCoordinationError, NonExistentAngleError, \
+from lammps_data.uff_table import UffTable, \
+                                  NonExistentElementError, \
+                                  NonExistentCoordinationError, \
+                                  NonExistentAngleError, \
                                   LookupMultiplicityError
 
 
@@ -25,19 +27,17 @@ def test_lookup_of_nonexistent_angle_should_raise_an_exception(uff_table):
 
 
 def test_lookup_of_matching_angle_should_return_the_row(uff_table):
-    expected_row = dict(type='C_1', bond=0.706, angle=180.0, distance=3.851, energy=0.105, scale=12.73, charge=1.912)
+    expected_row = dict(type='C_1', element='C', bond=0.706, angle=180.0, distance=3.851,
+                        energy=0.105, scale=12.73, charge=1.912, coordination=2)
     assert uff_table.lookup('C', 2, 180, tolerance=0) == expected_row
 
 
 def test_lookup_of_angle_within_tolerance_should_return_the_row(uff_table):
-    expected_row = dict(type='C_1', bond=0.706, angle=180.0, distance=3.851, energy=0.105, scale=12.73, charge=1.912)
+    expected_row = dict(type='C_1', element='C', bond=0.706, angle=180.0, distance=3.851,
+                        energy=0.105, scale=12.73, charge=1.912, coordination=2)
     assert uff_table.lookup('C', 2, 175, tolerance=10) == expected_row
 
 
-def test_lookup_with_angle_multiplicity_using_low_tolerance_should_return_single_row():
-    assert len(uff_table.lookup('O', 4, 104.51, tolerance=0)) == 1
-
-
-def test_lookup_with_angle_multiplicity_using_high_tolerance_should_raise_an_exception():
+def test_lookup_with_angle_multiplicity_using_high_tolerance_should_raise_an_exception(uff_table):
     with pytest.raises(LookupMultiplicityError):
         uff_table.lookup('O', 4, 104.51, tolerance=50)
